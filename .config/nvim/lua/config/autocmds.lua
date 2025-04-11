a78096f96f6b7d6ca19ec40c120582653b016d2b
x626f <26394523+x626f@users.noreply.github.com>
2025-04-11 12:57:47 -0700

    Simplify our setup, remove: neotree, matching brackets popup in CursorMoved

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -35,48 +35,48 @@ vim.api.nvim_create_autocmd("BufDelete", {
 	end,
 })
 
-vim.api.nvim_create_autocmd("CursorMoved", {
-	pattern = "*",
-	callback = function()
-		local cur_pos = vim.api.nvim_win_get_cursor(0)
-		local col = cur_pos[2]
-		local char = vim.api.nvim_get_current_line():sub(col + 1, col + 1)
-		if not char:match("[%(%){%[%]%)]") then
-			return
-		end
-
-		-- Jump to match, get pos, return
-		vim.cmd("normal! %")
-		local match_pos = vim.api.nvim_win_get_cursor(0)
-		vim.api.nvim_win_set_cursor(0, cur_pos)
-
-		-- Check if off-screen
-		local match_line = match_pos[1]
-		local top = vim.fn.line("w0")
-		local bottom = vim.fn.line("w$")
-		if match_line >= top and match_line <= bottom then
-			return
-		end
-
-		-- Popup
-		local msg = "Match at line " .. match_line .. ": " .. vim.fn.getline(match_line)
-		local buf = vim.api.nvim_create_buf(false, true)
-		vim.api.nvim_buf_set_lines(buf, 0, -1, false, { msg })
-		local opts = {
-			relative = "cursor",
-			row = 1,
-			col = 1,
-			width = #msg + 2,
-			height = 1,
-			style = "minimal",
-			border = "single",
-		}
-		local win = vim.api.nvim_open_win(buf, false, opts)
-		vim.defer_fn(function()
-			vim.api.nvim_win_close(win, true)
-		end, 1500)
-	end,
-})
+-- vim.api.nvim_create_autocmd("CursorMoved", {
+-- 	pattern = "*",
+-- 	callback = function()
+-- 		local cur_pos = vim.api.nvim_win_get_cursor(0)
+-- 		local col = cur_pos[2]
+-- 		local char = vim.api.nvim_get_current_line():sub(col + 1, col + 1)
+-- 		if not char:match("[%(%){%[%]%)]") then
+-- 			return
+-- 		end
+--
+-- 		-- Jump to match, get pos, return
+-- 		vim.cmd("normal! %")
+-- 		local match_pos = vim.api.nvim_win_get_cursor(0)
+-- 		vim.api.nvim_win_set_cursor(0, cur_pos)
+--
+-- 		-- Check if off-screen
+-- 		local match_line = match_pos[1]
+-- 		local top = vim.fn.line("w0")
+-- 		local bottom = vim.fn.line("w$")
+-- 		if match_line >= top and match_line <= bottom then
+-- 			return
+-- 		end
+--
+-- 		-- Popup
+-- 		local msg = "Match at line " .. match_line .. ": " .. vim.fn.getline(match_line)
+-- 		local buf = vim.api.nvim_create_buf(false, true)
+-- 		vim.api.nvim_buf_set_lines(buf, 0, -1, false, { msg })
+-- 		local opts = {
+-- 			relative = "cursor",
+-- 			row = 1,
+-- 			col = 1,
+-- 			width = #msg + 2,
+-- 			height = 1,
+-- 			style = "minimal",
+-- 			border = "single",
+-- 		}
+-- 		local win = vim.api.nvim_open_win(buf, false, opts)
+-- 		vim.defer_fn(function()
+-- 			vim.api.nvim_win_close(win, true)
+-- 		end, 1500)
+-- 	end,
+-- })
 
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
