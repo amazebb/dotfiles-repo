ed960fd32b0e226ac2f0046a8b59b1da4b1be142
x626f <26394523+x626f@users.noreply.github.com>
2025-04-14 15:36:00 -0700

    Fix issue #1: dotfiles now works correctly within tracked folders and there sub-folders

--- .local/scripts/dotfiles.sh
+++ .local/scripts/dotfiles.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 # Used for adding dotfiles in home directory to git bare repo
+# File: /Users/x626f/.local/scripts/dotfiles.sh
 
 DOTFILES_TRACKED_FOLDERS=("$HOME/.local/scripts" "$HOME/.config/nvim")
+
+# Check if PWD is HOME or within DOTFILES_TRACKED_FOLDERS
 if [[ "$PWD" == "$HOME" ]]; then
+  is_tracked=1
+else
+  is_tracked=0
+  for folder in "${DOTFILES_TRACKED_FOLDERS[@]}"; do
+    if [[ "$PWD" == "$folder" || "$PWD" == "$folder"/* ]]; then
+      is_tracked=1
+      break
+    fi
+  done
+fi
+
+if [[ "$1" == "git-cmd" ]]; then
+  # is_git_repo=$(git rev-parse --is-inside-work-tree 2>/dev/null && echo "true" || echo "false")
+  if [[ $is_tracked -eq 1 ]]; then
+    echo "/Users/x626f/.local/scripts/dotfiles.sh"
+  elif git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    echo "/opt/homebrew/bin/git"
+  else
+    echo ""
+  fi
+  exit 0
+fi
+
+if [[ $is_tracked -eq 1 ]]; then
   if [[ "$1" == "status" ]]; then
     if [[ "$2" == "--porcelain" ]]; then
       git --git-dir="$HOME/.git" --work-tree="$HOME" status --porcelain
@@ -13,6 +40,7 @@ if [[ "$PWD" == "$HOME" ]]; then
       if [[ -n "$untracked" ]]; then
         printf "\n%s\n%s\n" "Untracked files in tracked folders:" '(use "git add <file>..." to include in what will be committed)'
         echo -e "\033[31m"
+        # shellcheck disable=SC2001
         echo "$untracked" | sed 's/^/\t/'
         echo -e "\033[0m"
       fi
