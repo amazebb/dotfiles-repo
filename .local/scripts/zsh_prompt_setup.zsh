60816199715cf787cc53a11fb01b9c9a34c546d9
x626f <26394523+x626f@users.noreply.github.com>
2025-04-17 17:02:21 -0700

    Fixes #18: RPS1 aligned to the right

--- .local/scripts/zsh_prompt_setup.zsh
+++ .local/scripts/zsh_prompt_setup.zsh
@@ -6,24 +6,30 @@ setup_prompt() {
   betz_content_vcs=""
 
   prompt_segment() {
-    k='%K{blue}'
-    echo -n "$k%50<..<%~"
+    # We need to wrap in %{ %} to ensure the %K{blue} is not intpereted as spaces
+    # If we dont it will counted towards the character length of the prompt,
+    # thus messing up the internal logic of zsh and not padding the RPS1 flusg
+    # to the right ahnd side of the terminal
+    k='%{%K{blue}%}'
+    echo -n "$k%60<..<%~"
   }
 
   prompt_segment_tail() {
-    s='%F{blue}\uE0B0'
+    s='%{%F{blue}%}\uE0B0'
     # Prevent Mac terminal brightening font color with no background
-    # https://apple.stackexchange.com/questions/282911/prevent-mac-terminal-brightening-font-color-with-no-background/446604#446604
-    n='\e[48;5;256m' # set background to an out of range value, %K{256} does not work
+    # https://apple.stackexchange.com/questions/282911/
+    # prevent-mac-terminal-brightening-font-color-with-no-background/446604#446604
+    # We set background to an out of range value works, %k and %K{256} do not work
+    n='%{\e[48;5;256m%}'
     if [[ -n "${betz_content_vcs}" ]]; then
-      local branch="${betz_content_vcs%% *}"        # Get branch name before modifiers
-      local modifiers="${betz_content_vcs#$branch}" # Get modifiers (+/*/)
+      local branch="${betz_content_vcs%% *}"
+      local modifiers="${betz_content_vcs#$branch}"
       if [[ "${modifiers}" =~ "[+~?]" ]]; then
-        b='%K{yellow}'
-        f='%F{yellow}'
+        b='%{%K{yellow}%}'
+        f='%{%F{yellow}%}'
       else
-        b='%K{green}'
-        f='%F{green}'
+        b='%{%K{green}%}'
+        f='%{%F{green}%}'
       fi
       echo -n "$b$s %f${betz_content_vcs}$f $n\uE0B0%f%k"
     else
@@ -61,11 +67,11 @@ setup_prompt() {
       unstaged=$(echo "$porcelain" | grep -c "^.[MTDRC]")
       untracked=$(echo "$porcelain" | grep -c "^??")
     fi
-    local dirty=$((unstaged + staged))
+    local dirty=$((unstaged + staged + untracked))
 
     # Build VCS string similar to p10k
     local seg="$branch"
-    if [[ $dirty -gt 0 || $untracked -gt 0 ]]; then
+    if [[ $dirty -gt 0 ]]; then
       [[ $staged -gt 0 ]] && seg="${seg} +$staged"
       [[ $unstaged -gt 0 ]] && seg="${seg} ~$unstaged"
       [[ $untracked -gt 0 ]] && seg="${seg} ?$untracked"
@@ -88,13 +94,8 @@ setup_prompt() {
 
   # Update right prompt dynamically
   rprompt_segment_dynamic() {
-    TIME='%D{%H:%M}' # Time in 24h format
-    RPS1="%(?.%F{green}✔.%F{red}✘)%f%k %k$TIME%f%k"
+    RPS1="%(?.%{%F{green}%}✔.%{%F{red}%}✘)%f%k %k%*%f%k"
   }
-
-  # Set initial PS1 and RPS1
-  PS1=""
-  RPS1=""
 
   # Hook precmd to update prompt
   precmd_functions+=(precmd_prompt rprompt_segment_dynamic)
