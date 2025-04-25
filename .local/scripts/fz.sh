280b2bfa9b15593b80fa7d2ea20488c15513095a
x626f <26394523+x626f@users.noreply.github.com>
2025-04-25 10:03:27 -0700

    Fixes #29: fz.sh improved search allows to toggle rg and fzf and bat for preview

--- .local/scripts/fz.sh
+++ .local/scripts/fz.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/opt/homebrew/bin/bash
 
-# fz.sh: Search for text in files
-
-# Version
+# Search for text in files interactively using ripgrep and fzf
 VERSION="1.0.0"
 
 # Help function
@@ -63,5 +61,30 @@ echo "folders: $FOLDERS"
 echo "-r: $RG_OPTIONS"
 echo "-z: $FZF_OPTIONS"
 
-# Add your script logic below
-rg --no-messages --files-with-matches "${PATTERN}" "${FOLDERS}" | fzf --multi --style full --preview "rg --pretty --context 0 $PATTERN {}"
+# This is taken from https://junegunn.github.io/fzf/tips/ripgrep-integration/
+RELOAD="reload:rg --column --line-number --no-heading --color=always --smart-case {q} || :"
+
+# shellcheck disable=SC2016
+OPENER='if [[ $FZF_SELECT_COUNT -eq 0 ]]; then
+            nvim {1} +{2}     # No selection. Open the current line in Vim.
+          else
+            nvim +cw -q {+f}  # Build quickfix list for the selected items.
+          fi'
+
+# shellcheck disable=SC2016
+fzf --disabled --ansi --multi \
+  --bind "start:$RELOAD" --bind "change:$RELOAD" \
+  --bind "enter:become:$OPENER" \
+  --bind "ctrl-o:execute:$OPENER" \
+  --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
+  --bind 'ctrl-t:transform:[[ ! $FZF_PROMPT =~ ripgrep ]] &&
+      echo "rebind(change)+change-prompt(1. ripgrep> )+disable-search+transform-query:echo \{q} > /tmp/rg-fzf-f; cat /tmp/rg-fzf-r" ||
+      echo "unbind(change)+change-prompt(2. fzf> )+enable-search+transform-query:echo \{q} > /tmp/rg-fzf-r; cat /tmp/rg-fzf-f"' \
+  --prompt '1. ripgrep> ' \
+  --delimiter : \
+  --header 'CTRL-T: Switch between ripgrep/fzf' \
+  --style full \
+  --layout=reverse-list \
+  --preview 'bat --theme=auto:system --style=full --wrap=character --color=always --highlight-line {2} {1}' \
+  --preview-window '~4,+{2}+4/3,<80(up)' \
+  --query "$PATTERN"
