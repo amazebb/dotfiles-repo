1c2ec50067b99693c22aea35941f4454360c1c25
x626f <26394523+x626f@users.noreply.github.com>
2025-05-06 09:48:59 -0700

    Fixes #48: Rename tools.lua to plugins.lua, merge ui.lua and then remove ui.lua

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -71,4 +71,33 @@ return {
       })
     end,
   },
+  {
+    "luukvbaal/nnn.nvim",
+    config = function() require("nnn").setup() end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "solarized_dark",
+          component_separators = "|",
+          section_separators = { left = "", right = "" },
+        },
+        tabline = {
+          lualine_a = {
+            {
+              "buffers",
+              show_filename_only = true,
+              show_modified_status = true,
+              icons_enabled = false,
+              separator = "|",
+              symbols = { alternate_file = "", directory = "" },
+            },
+          },
+        },
+      })
+    end,
+  },
 }
