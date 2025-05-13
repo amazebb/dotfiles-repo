ab6e5829eacb814439dbee52726194df74d1a177
x626f <26394523+x626f@users.noreply.github.com>
2025-05-13 15:53:58 -0700

    Removed old config/setup syntax to recommended opts for plugins.lua

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -3,31 +3,30 @@ return {
 	{
 		'nvim-treesitter/nvim-treesitter',
 		build = ':TSUpdate',
-		config = function()
-			require('nvim-treesitter.configs').setup({
-				ensure_installed = {
-					'bash',
-					'c',
-					'html',
-					'javascript',
-					'latex',
-					'lua',
-					'markdown',
-					'markdown_inline',
-					'python',
-					'sql',
-					'vim',
-					'vimdoc',
-					'yaml',
-				},
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },
-				ignore_install = {},
-				modules = {},
-				auto_install = true,
-			})
-		end,
+		opts = {
+			ensure_installed = {
+				'bash',
+				'c',
+				'go',
+				'html',
+				'javascript',
+				'latex',
+				'lua',
+				'markdown',
+				'markdown_inline',
+				'python',
+				'sql',
+				'vim',
+				'vimdoc',
+				'yaml',
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+			ignore_install = {},
+			modules = {},
+			auto_install = true,
+		},
 	},
 
 	-- Markdown renderer
@@ -36,13 +35,8 @@ return {
 		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
 		---@module 'render-markdown'
 		---@type render.md.UserConfig
-		opts = {},
+		opts = { completions = { lsp = { enabled = true } } },
 		ft = { 'markdown', 'quarto' },
-		config = function()
-			require('render-markdown').setup({
-				completions = { lsp = { enabled = true } },
-			})
-		end,
 	},
 
 	-- fzf Neovim
@@ -61,20 +55,38 @@ return {
 	{
 		'olimorris/codecompanion.nvim',
 		dependencies = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter', 'ravitemer/mcphub.nvim' },
-		config = function()
-			require('codecompanion').setup({
-				extensions = {
-					mcphub = {
-						callback = 'mcphub.extensions.codecompanion',
-						opts = {
-							make_vars = true,
-							make_slash_commands = true,
-							show_result_in_chat = true,
+		opts = {
+			adapters = {
+				xai = function()
+					return require('codecompanion.adapters').extend('xai', {
+						env = {
+							api_key = 'cmd:security find-generic-password -s xai-api-key -w 2>/dev/null',
 						},
+					})
+				end,
+			},
+			extensions = {
+				mcphub = {
+					callback = 'mcphub.extensions.codecompanion',
+					opts = {
+						make_vars = true,
+						make_slash_commands = true,
+						show_result_in_chat = true,
 					},
 				},
-			})
-		end,
+			},
+		},
+		strategies = {
+			chat = {
+				adapter = 'xai',
+			},
+			inline = {
+				adapter = 'xai',
+			},
+			cmd = {
+				adapter = 'xai',
+			},
+		},
 	},
 
 	-- MCP
@@ -83,64 +95,55 @@ return {
 		dependencies = {
 			'nvim-lua/plenary.nvim', -- Required for Job and HTTP requests
 		},
-		-- uncomment the following line to load hub lazily
-		--cmd = "MCPHub",  -- lazy load
+		opts = {},
+		cmd = 'MCPHub', -- lazy load
 		build = 'npm install -g mcp-hub@latest', -- Installs required mcp-hub npm module
 		-- uncomment this if you don't want mcp-hub to be available globally or can't use -g
 		-- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
-		config = function()
-			require('mcphub').setup()
-		end,
 	},
 
 	-- Manage LSPs
 	{
 		'williamboman/mason.nvim',
-		config = function()
-			require('mason').setup({
-				ui = {
-					icons = {
-						package_installed = '✓',
-						package_pending = '➜',
-					},
-					border = 'rounded',
+		opts = {
+			ui = {
+				icons = {
+					package_installed = '✓',
+					package_pending = '➜',
 				},
-			})
-		end,
+				border = 'rounded',
+			},
+		},
 	},
 
 	-- nnn File Explorer
 	{
 		'luukvbaal/nnn.nvim',
-		config = function()
-			require('nnn').setup()
-		end,
+		opts = {},
 	},
 
 	--  Neovim statusline
 	{
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
-		config = function()
-			require('lualine').setup({
-				options = {
-					theme = 'solarized_dark',
-					component_separators = '|',
-					section_separators = { left = '', right = '' },
-				},
-				tabline = {
-					lualine_a = {
-						{
-							'buffers',
-							show_filename_only = true,
-							show_modified_status = true,
-							icons_enabled = false,
-							separator = '|',
-							symbols = { alternate_file = '', directory = '' },
-						},
+		opts = {
+			options = {
+				theme = 'solarized_dark',
+				component_separators = '|',
+				section_separators = { left = '', right = '' },
+			},
+			tabline = {
+				lualine_a = {
+					{
+						'buffers',
+						show_filename_only = true,
+						show_modified_status = true,
+						icons_enabled = false,
+						separator = '|',
+						symbols = { alternate_file = '', directory = '' },
 					},
 				},
-			})
-		end,
+			},
+		},
 	},
 }
