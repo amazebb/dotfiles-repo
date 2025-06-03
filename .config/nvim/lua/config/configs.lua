556d1793a7b611fd0a9b5d0d8f6d676e4559d488
x626f <26394523+x626f@users.noreply.github.com>
2025-06-03 11:51:00 -0700

    chore(nvim): conditionally set terminal colors based on TERM environment

--- .config/nvim/lua/config/configs.lua
+++ .config/nvim/lua/config/configs.lua
@@ -22,10 +22,15 @@ vim.diagnostic.config({
 vim.api.nvim_set_hl(0, 'CursorLineNr', { ctermfg = 'Yellow' })
 vim.api.nvim_set_hl(0, 'LineNr', { ctermbg = 'DarkGray', ctermfg = 'Black', bold = false })
 vim.api.nvim_set_hl(0, 'MatchParen', { standout = true })
-vim.api.nvim_set_hl(0, 'NormalFloat', { ctermbg = 'Black' })
+
+if os.getenv('TERM') == 'xterm-256color' then
+    vim.api.nvim_set_hl(0, 'NormalFloat', { ctermbg = 'Black' })
+    vim.api.nvim_set_hl(0, 'PmenuSel', { fg = 'white', bold = true, reverse = true })
+else
+    vim.opt.termguicolors = true
+end
 vim.api.nvim_set_hl(0, 'Comment', { ctermfg = 'DarkGray', italic = true })
 vim.api.nvim_set_hl(0, 'CurrentTag', { underline = true, bold = true, standout = true })
-vim.api.nvim_set_hl(0, 'PmenuSel', { fg = 'white', bold = true, reverse = true })
 
 -- yank to the clipboard automatically
 vim.opt.clipboard:append({ 'unnamedplus' })
