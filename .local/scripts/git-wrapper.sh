58743a4af6ff54305430be49783d30e71ce12c43
x626f <26394523+x626f@users.noreply.github.com>
2025-06-13 11:37:36 -0700

    Added git-wrapper.sh to make merge with feat work

---
+++ .local/scripts/git-wrapper.sh
@@ -0,0 +1,58 @@
+#!/opt/homebrew/bin/bash
+# Manage dotfiles with Git bare repo.
+
+# <<< is a here-string in Bash
+# Feeds a string directly to a command's stdin
+# Equivalent to: echo "$(git config ...)" | IFS=',' read -r -a tracked_files
+# More efficient, avoids pipe/subshell
+IFS=',' read -r -a DOTFILES_TRACKED_FOLDERS <<<"$(git config --global --get dotfiles.tracked-folders)"
+
+# Check if PWD is HOME or within DOTFILES_TRACKED_FOLDERS
+if [[ $PWD == "$HOME" ]]; then
+  is_tracked=1
+else
+  is_tracked=0
+  for folder in "${DOTFILES_TRACKED_FOLDERS[@]}"; do
+    folder=${folder/#\~/$HOME}
+    if [[ $PWD == "$folder" || $PWD == "$folder"/* ]]; then
+      is_tracked=1
+      break
+    fi
+  done
+fi
+
+if [[ $1 == "git-cmd" ]]; then
+  if [[ $is_tracked -eq 1 ]]; then
+    echo "$HOME/.local/scripts/dotfiles.sh"
+  elif [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) == "true" ]]; then
+    echo "/opt/homebrew/bin/git"
+  else
+    echo ""
+  fi
+  exit 0
+fi
+
+if [[ $is_tracked -eq 1 ]]; then
+  if [[ $1 == "status" ]]; then
+    if [[ $2 == "--porcelain" ]]; then
+      git --git-dir="$HOME/.git" --work-tree="$HOME" status --porcelain
+      git --git-dir="$HOME/.git" --work-tree="$HOME" ls-files --others --exclude-standard "${DOTFILES_TRACKED_FOLDERS[@]}" | sed 's/^/?? /'
+    else
+      git --git-dir="$HOME/.git" --work-tree="$HOME" "$@"
+      untracked=$(git --git-dir="$HOME/.git" --work-tree="$HOME" ls-files --others --exclude-standard "${DOTFILES_TRACKED_FOLDERS[@]}")
+      if [[ -n $untracked ]]; then
+        printf "\n%s\n%s\n" "Untracked files in tracked folders:" '(use "git add <file>..." to include in what will be committed)'
+        echo -e "\033[31m"
+        # shellcheck disable=SC2001
+        echo "$untracked" | sed 's/^/\t/'
+        echo -e "\033[0m"
+      fi
+    fi
+  elif [[ $1 == "clean" ]]; then
+    git --git-dir="$HOME/.git" --work-tree="$HOME" clean "$@" "${DOTFILES_TRACKED_FOLDERS[@]}"
+  else
+    git --git-dir="$HOME/.git" --work-tree="$HOME" "$@"
+  fi
+else
+  git "$@"
+fi
