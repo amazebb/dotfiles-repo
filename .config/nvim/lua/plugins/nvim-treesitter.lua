bdec34af1675db0594ae3f0870381962e0de0e95
x626f <-----------------@icloud.com>
2025-10-01 14:41:13 -0700

    feat(nvim-treesitter): replace opts with config function and add parser installations

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,14 +3,42 @@ return {
     lazy = false,
     branch = "master",
     build = ":TSUpdate",
-    opts = {
-        highlight = {
-            enable = true,
-            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-            -- Using this option may slow down your editor, and you may see some duplicate highlights.
-            -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = false,
-        },
-    },
+    config = function()
+        ---@diagnostic disable-next-line: missing-fields
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "awk",
+                "bash",
+                "c",
+                "fortran",
+                "lua",
+                "vim",
+                "vimdoc",
+                "markdown",
+                "markdown_inline",
+                "typescript",
+                "tsx",
+                "python",
+                "html",
+                "css",
+                "java",
+                "javascript",
+                "julia",
+                "query",
+                "sql",
+            },
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
+
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
+
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            incremental_selection = { enable = true },
+        })
+    end,
 }
